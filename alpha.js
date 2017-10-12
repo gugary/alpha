aca4df1ed8ff81db064e65f08ff09018b0bbc060
Gary Gu <gu.gary@gmail.com>
2017-10-12 12:47:05 -0400

    series

--- alpha.js
+++ alpha.js
@@ -4,9 +4,12 @@
             url: 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=ZGFR4Q6NTMDFZQ2A',
             dataType: 'json',
             cache: false,
-            success: function(json){
-		const obj = JSON.parse(json);
-		alert(obj["Meta Data"]);
+            success: function(obj){
+		const meta=obj["Meta Data"];
+		const last=meta["3. Last Refreshed"];
+		const onem = obj["Time Series (1min)"];
+		const price= onem[last];
+		alert(price["4. close"]);
             },
             error: function(xhr, status, err){
                 console.error(status, err.toString());
